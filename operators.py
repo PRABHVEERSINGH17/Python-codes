05411cdb4d6b375fee335d113de77cddc299c0de
Prabhveer singh <146875097+PRABHVEERSINGH17@users.noreply.github.com>
2026-01-05 02:20:26 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,35 @@
+a = 2
+b = 2
+c = a + b
+print(c)
+# This code adds two numbers and prints the result
+# Expected output: 4
+# Additional comment: Simple addition operation
+# Another comment: Variables a and b are both set to 2
+# End of code
+# This is a simple addition operation
+# that demonstrates the use of variables
+# and the print function in Python.
+# The result of adding a and b is stored in c
+# and then printed to the console.
+# The expected output of this code is 4
+# since both a and b are equal to 2.
+a=4
+b=9
+b+=2#incrementation of value of b by 2
+print(b)
+b-=7#decrementation of value of b by 7
+print(b)
+##comparison operators
+d = 5>4
+print(d)
+e = 4!=4
+print(e)
+# Additional comment: The code also demonstrates augmented assignment operators
+# and comparison operators such as greater than and not equal to.       
+e = 10>23
+print(e)
+m = 34<=67
+print(m)
+# This code demonstrates augmented assignment operators
+# and comparison operators such as greater than and not equal to.
