659f4364d97c3683af6149d8af609ca6d6a634b9
Prabhveer singh <146875097+PRABHVEERSINGH17@users.noreply.github.com>
2026-01-04 19:47:55 +0530

    Add files via upload

---
+++ experiment8.py
@@ -0,0 +1,12 @@
+a=1 # a is a float
+b=2.2#b is a floating point number
+c = "Harry" # c is a string
+d = 'H' # d is a character
+e = True # e is a boolean
+f = None # f is a NoneType
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
