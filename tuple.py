92895799491d5d5640e0ea74440b6cf88ac3221c
Prabhveer singh <146875097+PRABHVEERSINGH17@users.noreply.github.com>
2026-01-11 15:27:44 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,17 @@
+a = (1,2,3,4,5,6,6,7,7,8,9)
+print(type(a))
+no = a.count(7)
+print(no)
+index = a.index(6)
+print(index)
+##Methods in tuple##
+print(len(a))
+
+
+print(max(a))
+
+print(min(a))
+print(sum(a))
+
+print(sorted(a))
+print(reversed(a))
